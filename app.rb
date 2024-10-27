99aecf9faf4f8f74a66f4476dee4c8f1f0a8503f
jake77895 <jspitzer@chicagobooth.edu>
2024-10-27 05:11:41 +0000

    Commit

--- app.rb
+++ app.rb
@@ -1,6 +1,40 @@
 require "sinatra"
 require "sinatra/reloader"
 
+
+get("/payment/results") do
+
+    # Retrieve values from the query string
+    apr = params["apr"].to_f
+    years = params["years"].to_i
+    principal = params["principal"].to_f
+  
+    # Calculate the payment using the formula
+    payment = (apr / 100 * principal) / (1 - (1 + apr / 100) ** -years)
+  
+    # Pass the values to the view
+    erb :payment_results, locals: { apr: apr, years: years, principal: principal, payment: payment.round(2) }
+
+end
+
+get("/payment/new") do
+
+  erb(:payment)
+
+end
+
+get("/square_root/results") do
+
+  erb(:square_root_results)
+
+end
+
+get("/square_root/new") do
+
+  erb(:square_roots)
+
+end
+
 get("/square/results") do
 
   erb(:square_results)
